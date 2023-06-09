3501b9a18649db1d143b0c820f58df5ad26f5fff
Fernando Poli <fernandopoli@MacBook-Pro-de-Fernando>
2023-06-09 11:26:52 -0300

    Testing.

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-// Add Express
-const express = require("express");
-// Initialize Express
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
 const app = express();
-// Create GET request
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+
+app.get('/', async (req, res) => {
+  try {
+    // Hacer la solicitud HTTP a la página web
+    const response = await axios.get('https://www.rosario3.com/');
+
+    // Analizar el HTML de la página web
+    const $ = cheerio.load(response.data);
+
+    // Extraer los datos necesarios
+    const datos = $('.left-content .title').text();
+
+    // Enviar los datos en la respuesta
+    res.json({ datos });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Ocurrió un error al obtener los datos.' });
+  }
 });
-// Initialize server
-app.listen(5000, () => {
-  console.log("Running on port 5000.");
+
+// Iniciar el servidor
+// const port = 3000;
+app.set('port', process.env.PORT || 3000)
+
+app.listen(app.get('port'), () => {
+  console.log(`Servidor en funcionamiento en el puerto ${app.get('port')}`);
 });
+
+// Export the Express API
+module.exports = app;
