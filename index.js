bb88d24c208e180a8961df8112f7522782b5b125
Fernando Poli <fernandopoli@MacBook-Pro-de-Fernando>
2023-06-09 15:49:35 -0300

    Testing.

--- index.js
+++ index.js
@@ -4,19 +4,43 @@ const cheerio = require('cheerio');
 
 const app = express();
 
+// const fetchTitle = async () => {
+//   try{
+//     const response = await axios.get('https://www.rosario3.com/');
+//     const html = response.data;
+//     const $ = cheerio.load(html);
+//     const titles = [];
+
+//     $('.left-content .title').each((index, el) =>{
+//         const title = el;
+//         titles.push($(title).text())
+//       });
+
+//     return titles;
+//   }
+//   catch (err){
+//     console.log(err);
+//   }
+// }
+
+// fetchTitle().then(titles => console.log(titles));
+
 app.get('/', async (req, res) => {
   try {
     // Hacer la solicitud HTTP a la página web
     const response = await axios.get('https://www.rosario3.com/');
 
     // Analizar el HTML de la página web
-    const $ = cheerio.load(response.data);
-
-    // Extraer los datos necesarios
-    const datos = $('.left-content .title').text();
-
-    // Enviar los datos en la respuesta
-    res.json({ datos });
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const noticias = [];
+    
+    $('.left-content .title').each((index, dato) =>{      
+      noticias.push($(dato).text())
+    });
+    
+    // Enviar los noticias en la respuesta
+    res.json({ noticias });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Ocurrió un error al obtener los datos.' });
@@ -30,6 +54,3 @@ app.set('port', process.env.PORT || 3000)
 app.listen(app.get('port'), () => {
   console.log(`Servidor en funcionamiento en el puerto ${app.get('port')}`);
 });
-
-// Export the Express API
-module.exports = app;
